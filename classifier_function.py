1fb33fb11f7af4100b929c224b344d7aa37efbd7
Me <me@mes-macbook-pro.lan>
2020-06-15 16:45:29 -0400

    Code that sorts array for use in confusion matrix

--- classifier_function.py
+++ classifier_function.py
@@ -11,8 +11,7 @@ from keras.preprocessing import image
 from keras.models import load_model
 import os
 
-#directory where images to be categorized live
-directory = ''
+
 
 #Helper function
 def get_files(directory):
@@ -26,6 +25,7 @@ def get_files(directory):
 def image_classifier(image_path, model_path):
     classifier = load_model(model_path)
     image_list = get_files(image_path)
+    #print(image_list)
     prediction_list = []
     for i in range(len(image_list)):
         test_image = image.load_img(image_list[i], target_size = (64, 64))
@@ -36,3 +36,43 @@ def image_classifier(image_path, model_path):
         prediction_list.append(result)
     out_list = dict(zip(image_list, prediction_list))
     return out_list
+
+#standard code to implement functions
+if __name__ == '__main__':
+    #point code to the directory where the images are 
+    directory = '/Users/me/dev/CNN-exercise/test_set/'
+    
+    #direct the computer to the model path
+    model_path = '/Users/me/dev/CNN-exercise/CNN_model.h5'
+    
+    #code to pull the outlist by running the image classifier on the directory, using the model
+    out_list = image_classifier(directory,model_path)
+    print(out_list)
+
+    #set up zeroed-out true positive, etc counters so can add to them and determine the total numberss
+    number_of_TP = 0
+    number_of_FN = 0
+    number_of_FP = 0
+    number_of_TN = 0
+
+    #out_list is a dictionary which contains the image file as the key and the prediction number as the value.
+    #the "for loop" iterates through each item, and looks at whether the key contains "not_tank" or "test_set/tank",
+    #then also checks if the value is 0 or anything 1 or greater.  It then adds 1 each time if finds an iterable that
+    #matches both those conditions to the appropriate true positive, etc number.  Then it prints the total numbers.
+    for image_file, number in list(out_list.items()):
+        if 'not_tank' in image_file and number < 1:
+            #print(image_file)
+            #print(number)
+            number_of_TN += 1
+        if 'not_tank' in image_file and number >= 1:
+            number_of_FP += 1
+        if 'test_set/tank' in image_file and number < 1:
+            number_of_FN += 1
+        if 'test_set/tank' in image_file and number >= 1:
+            number_of_TP += 1
+
+    print("There were {} number of True Positives".format(number_of_TP))
+    print("There were {} number of False Negatives".format(number_of_FN))
+    print("There were {} number of False Positives".format(number_of_FP))
+    print("There were {} number of True Negatives".format(number_of_TN))
+
